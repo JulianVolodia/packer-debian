60c16844ede8384623c19d9bea574b6f3b89b129
JulianVolodia <julianvolodia@gmail.com>
2015-06-15 09:44:23 +0200

    Jessie prepared 'Veewee' way

--- scripts/jessie/networking.sh
+++ scripts/jessie/networking.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 if [ $(facter virtual) = 'virtualbox' ] ; then
-    rm /etc/udev/rules.d/70-persistent-net.rules
-    mkdir /etc/udev/rules.d/70-persistent-net.rules
-    rm /lib/udev/rules.d/75-persistent-net-generator.rules
-    rm -rf /dev/.udev/ /var/lib/dhcp/*
-    echo "pre-up sleep 2" >> /etc/network/interfaces
+    rm /etc/udev/rules.d/70-persistent-net.rules | true
+    mkdir /etc/udev/rules.d/70-persistent-net.rules | true
+    rm /lib/udev/rules.d/75-persistent-net-generator.rules | true
+    rm -rf /dev/.udev/ /var/lib/dhcp/* | true
+    echo "pre-up sleep 2" >> /etc/network/interfaces | true
 fi
--- scripts/jessie/vbaddguest.sh
+++ scripts/jessie/vbaddguest.sh
@@ -4,13 +4,14 @@
 mkdir /tmp/vbox
 VER=$(cat /home/vagrant/.vbox_version)
 mount -o loop /home/vagrant/VBoxGuestAdditions_$VER.iso /tmp/vbox 
-yes | sh /tmp/vbox/VBoxLinuxAdditions.run
-umount /tmp/vbox
-rmdir /tmp/vbox
-rm /home/vagrant/*.iso
+
+yes | sh /tmp/vbox/VBoxLinuxAdditions.run | true
+umount /tmp/vbox | true
+rmdir /tmp/vbox | true
+rm /home/vagrant/*.iso | true
 ln -s /opt/VBoxGuestAdditions-*/lib/VBoxGuestAdditions /usr/lib/VBoxGuestAdditions
 
 # Cleanup
-rm -rf VBoxGuestAdditions_*.iso VBoxGuestAdditions_*.iso.?
-rm -rf /usr/src/virtualbox-ose-guest*
-rm -rf /usr/src/vboxguest*
+rm -rf VBoxGuestAdditions_*.iso VBoxGuestAdditions_*.iso.? | true
+rm -rf /usr/src/virtualbox-ose-guest* | true
+rm -rf /usr/src/vboxguest* | true
